b733cf7634a71bbf4a46d664608b68a8656d7a64
AlvaroRPrado <alvarorodrigues2010@gmail.com>
2024-11-18 12:56:52 -0300

    Mapeando classe de pra filmes populares

---
+++ app/src/main/java/com/prado/movieapp/core/data/remote/model/MovieResult.kt
@@ -0,0 +1,34 @@
+package com.prado.movieapp.core.data.remote.model
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieResult(
+    @SerializedName("adult")
+    val adult: Boolean,
+    @SerializedName("backdrop_path")
+    val backdropPath: String,
+    @SerializedName("genre_ids")
+    val genreIds: List<Int>,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("original_language")
+    val originalLanguage: String,
+    @SerializedName("original_title")
+    val originalTitle: String,
+    @SerializedName("page")
+    val overview: String,
+    @SerializedName("popularity")
+    val popularity: Double,
+    @SerializedName("poster_path")
+    val posterPath: String,
+    @SerializedName("release_date")
+    val releaseDate: String,
+    @SerializedName("video")
+    val title: String,
+    @SerializedName("page")
+    val video: Boolean,
+    @SerializedName("vote_average")
+    val voteAverage: Double,
+    @SerializedName("vote_count")
+    val voteCount: Int
+)
