ed338647b337f9c0404f51637f7d53b8b77966f1
AlvaroRPrado <alvarorodrigues2010@gmail.com>
2024-11-19 14:47:12 -0300

    Criando a interface MovieService

---
+++ app/src/main/java/com/prado/movieapp/core/data/remote/MovieService.kt
@@ -0,0 +1,34 @@
+package com.prado.movieapp.core.data.remote
+
+import android.graphics.Movie
+import com.prado.movieapp.core.data.remote.response.MovieDetaisResponse
+import com.prado.movieapp.core.data.remote.response.MovieResponse
+import com.prado.movieapp.core.data.remote.response.SearchResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface MovieService {
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies(
+        @Query("page") page: Int
+    ): MovieResponse
+
+    @GET("search/multi")
+    suspend fun searchMovie(
+        @Query("page") page: Int,
+        @Query("query") query: String
+    ): SearchResponse
+
+    @GET("movie/{movie_id}")
+    suspend fun getMovie(
+        @Path("movie_id") movieId: Int
+    ): MovieDetaisResponse
+
+    @GET("movie/{movie_id}/similar")
+    suspend fun getMoviesSimilar(
+        @Path("movie_id") movieId: Int,
+        @Query("page") page: Int
+    ): MovieResponse
+}
